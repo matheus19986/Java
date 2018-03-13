ab1fc8acc549e7b1360668ea1f39e1a7aae10ecb
Matheus Moraes <moraessmatheus0@hotmail.com>
2018-03-13 18:27:49 -0300

    Java

--- Revisao/src/revisao/Revisao.java
+++ Revisao/src/revisao/Revisao.java
@@ -15,8 +15,8 @@ public class Revisao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for ( int i =0; i< 5; i++){
-            System.out.println("*" + i++);
+        for ( int i =1; i< 6; i++){
+            System.out.println("* " + i);
         }
     }
     
