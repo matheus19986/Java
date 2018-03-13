39273257300c88a03b10640fabf70afc87df7f89
Matheus Moraes <moraessmatheus0@hotmail.com>
2018-03-13 18:50:03 -0300

    Exer02

---
+++ Revisao/src/revisao/Exer02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisao;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 10070319
+ */
+public class Exer02 { 
+    public static void main(String[] args) { 
+    Scanner scan = new Scanner  (System.in); 
+    
+        int num = 1;  
+        while(num != 0){
+        System.out.println("Digite um número: "); 
+        num = scan.nextInt(); 
+        System.out.println("Número: " + num); 
+        }
+           
+    
+         
+      } 
+    }
